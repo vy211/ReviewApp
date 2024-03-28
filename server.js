c7300b3d1a8530dec36c580e1c4ceb2675e88728
vy211 <vipinkumar.y@simplify3x.com>
2024-03-28 12:51:54 +0530

    middleware

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
-const bodyParser = require('body-parser');
-
+const eventRoutes = require("./routes/eventRoutes");
+const reviewRoutes = require("./routes/reviewRoutes");
+const authRoutes = require("./routes/authRoutes");
+const bodyParser = require("body-parser");
 
 //use supabase for backend its a famour RDBMS
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/users', userRoutes);
+app.use("/api", userRoutes);
+app.use("/api", eventRoutes);
+app.use("/api", reviewRoutes);
+app.use("/api", authRoutes);
+
 
 
 const PORT = process.env.PORT || 7000;
